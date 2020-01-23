ec23f914e3229631509980e780ab83673e083305
CAS OLEJNICZAK <CASOLEJNICZAK@GMAIL.COM>
2020-01-22 20:30:10 -0800

    implemented weather api

--- server.js
+++ server.js
@@ -13,40 +13,43 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 
+// ROUTES
 // route syntax = app.<operation>('route', callback);
+// Home page route for server testing
 app.get('/', (request, response) => {
   response.send('home page!!!!');
 });
 
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+// app.get('/events', eventsHandler);
+// app.get('/yelp', yelpHandler);
+// app.get('/movies', moviesHandler);
 
-// route for location/map
-app.get('/location', (request, response) => {
+
+// Location Functions
+function locationHandler(request, response) {
   try {
     // //Getting info for object
-
     const city = request.query.city;
-    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json&limit=1'`;
+    let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${city}&format=json&limit=1`;
 
     superagent.get(url)
       .then(data => {
         const geoData = data.body[0]; //first item
         const locationData = new Location(city, geoData);
         response.send(locationData);
-        console.log(geoData);
-
+        // console.log(geoData);
       })
 
   } catch (error) {
     errorHandler('it went wrong.', request, response);
-
   }
+}
 
-});
-//routes above function below
-
-//creating object
+// Location Object Constructor
 function Location(city, geoData) {
-  console.log('locationbuildobj');
+  // console.log('locationbuildobj');
   this.search_query = city;
   this.formatted_query = geoData.display_name;
   this.latitude = geoData.lat;
@@ -55,30 +58,31 @@ function Location(city, geoData) {
 
 //Route for weather
 
-app.get('/weather', (request, response) => {
+function weatherHandler(request, response) {
   try {
-    const weatherData = require('./darksky.json');
-    const forecastArray = [];
-    weatherData.daily.data.forEach(darkSky => {
-      const time = new Date(darkSky.time *1000).toString().slice(0, 15);
-      const forecast = darkSky.summary;
-      const weatherObj = new Weather(time, forecast);
-      forecastArray.push(weatherObj);
-    });
-    response.send(forecastArray);
+    const latitude = request.query.latitude;
+    const longitude = request.query.longitude;
+    let weatherURL = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`;
+    // console.log(weatherURL);
+
+    superagent.get(weatherURL)
+      .then(data => {
+        const forecastArray = data.body.daily.data.map(object => new Weather(object));
+        response.send(forecastArray);
+      })
 
   } catch (error) {
     errorHandler('something went wrong', request, response);
   }
-})
+}
 
 
 //constructor for weather
 
 
-function Weather(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
+function Weather(weatherObj) {
+  this.forecast = weatherObj.summary
+  this.time = new Date(weatherObj.time *1000).toString().slice(0, 15);
 }
 
 function errorHandler(error, request, response) {
